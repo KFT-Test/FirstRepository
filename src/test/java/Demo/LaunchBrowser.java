805f030f6a8757908e5ee08180115516123e4550
neha-chandan <nehamahajan199@gmail.com>
2019-10-08 14:11:40 +0530

    successful

---
+++ src/test/java/Demo/LaunchBrowser.java
@@ -0,0 +1,25 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+	
+WebDriver driver;
+	
+@BeforeSuite
+public void init() throws InterruptedException {
+System.setProperty("webdriver.chrome.driver", "D:\\Selenium course\\drivers\\chromedriver_win32\\chromedriver.exe");
+driver=new ChromeDriver();
+}
+
+@Test
+public void OpenUrl(){
+//String text;
+driver.get("http://cookbook.seleniumacademy.com/Locators.html");
+}
+
+}
